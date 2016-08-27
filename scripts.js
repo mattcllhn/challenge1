1c1b8a5f74370b6c990c9c376654c7bad8d50eea
matt <mattcllhn@gmail.com>
2016-08-27 17:57:49 -0500

    playing with a for in loop

--- scripts.js
+++ scripts.js
@@ -17,31 +17,38 @@ var addInfo = function(){
   lastName : document.getElementById('last').value,
   ID : document.getElementById('id').value,
   title : document.getElementById('title').value,
-  salary : Number(document.getElementById('salary').value)
+  salary : document.getElementById('salary').value
 };//end of employeeInfo object
 
 //forin loop that makes sure all fields are filled in and clears inputs
 
+for (var key in employeeInfo) {
+  if (employeeInfo[key].length==0) {
+      alert(key+' is a required field');
+      break;}//end of if
+    }
 
-// console.log(employeeInfo);
-  employeeList.push(employeeInfo);
-  // console.log(employeeList);
-                                                      // clears inputs
-  for (var i = 0; i < documentIds.length; i++) {
-  document.getElementById(documentIds[i]).value=""
-  }//end of clear inputs
-  empDisplay();
+    if(employeeInfo[key].length>0){
+      console.log('all is well');
+
+
+
+employeeList.push(employeeInfo);
+empDisplay();
+                                        // clears inputs
+
+  // for (var i = 0; i < documentIds.length; i++) {
+  // document.getElementById(documentIds[i]).value=""
+  // }//end of clear inputs
 };//end of addInfo
                                                   //appends information to the DOM
 var empDisplay=function(){
   document.getElementById('empDiv').innerHTML ="";
-  // console.log('in empDisplay');
   console.log(employeeList);
   for (var i = 0; i < employeeList.length; i++) {
     var emp=employeeList[i];
     var toHtml='<h2>'+emp.firstName+' '+emp.lastName+'</h2><br>ID Number: '+emp.ID+'</br><br>Title: '+emp.title+'</br><br>Salary: '+emp.salary+'</br>'
     var fireButton='<button onclick="fire()">Fire this jabroni</button>';
-
     document.getElementById('empDiv').innerHTML += toHtml;
     document.getElementById('empDiv').innerHTML +=fireButton;
     }//end of loop
@@ -61,7 +68,6 @@ var salaryCalculator=function(){
 
 
 var fire=function(index){
-// console.log(employeeList[index].name+' has been fired');
 employeeList.splice(index,1);
 empDisplay();
 };//end of fire
