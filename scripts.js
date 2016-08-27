3f6cb1762b929d06eab7952bb909f7851c48c840
matt <mattcllhn@gmail.com>
2016-08-27 14:04:02 -0500

    added emp object, clear inputs, add to array

--- scripts.js
+++ scripts.js
@@ -4,18 +4,31 @@
 // ID Number
 // Job Title
 // Annual Salary
-var firstName;
-var lastName;
-var ID;
-var title;
-var salary;
-console.log("test,test");
 
-var addName = function(){
-  console.log('in addName');
-  firstName = document.getElementById('first').value;
-  lastName = document.getElementById('last').value;
-  ID = document.getElementById('id').value;
-  title = document.getElementById('title').value;
-  salary = document.getElementById('salary').value;
-};
+console.log("test,test");
+var employeeList=[];
+var documentIds=['first','last','id','title','salary'];
+// var employeeInfo{firstname:,lastName:,ID:,title:,salary:};
+                                                //creates an object out of employe info,adds to array, clears inputs
+var addInfo = function(){
+  console.log('in addInfo');
+
+  var employeeInfo={
+  firstName : document.getElementById('first').value,
+  lastName : document.getElementById('last').value,
+  ID : document.getElementById('id').value,
+  title : document.getElementById('title').value,
+  salary : document.getElementById('salary').value
+};//end of employeeInfo object
+// console.log(employeeInfo);
+  employeeList.push(employeeInfo);
+  // console.log(employeeList);
+                                                      // clear inputs
+  for (var i = 0; i < documentIds.length; i++) {
+  document.getElementById(documentIds[i]).value=""
+  }//end of clear inputs
+};//end of addInfo function
+
+//appends information to the DOM
+//function that calculates salaries and reports monthly total cost
+//function that removes employee info from the DOM
